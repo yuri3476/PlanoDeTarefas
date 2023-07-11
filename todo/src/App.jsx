be02f9cb707b0ae8bcca0f4d0144c0e33f6ef64e
Yuri Rezende <yuri.rezende@souunit.com.br>
2023-07-11 10:28:54 -0300

    Criação campo de pesquisa

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Todo from './components/todo';
+import Todo from "./components/todo";
+import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
 
-import TodoForm from './components/TodoForm';
-
-import './App.css';
-
+import "./App.css";
+import Filter from "./components/Filter";
 
 function App() {
-
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -30,16 +29,82 @@ function App() {
     },
   ]);
 
-  return <div className="app">
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
+  // Adicionar uma nova tarefa
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  // Remover uma tarefa
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
+
+  // Completar uma tarefa
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo todo={todo}/>
-        ))}
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+
+          // Colocando em ordem crescente ou decrescente
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
-      <TodoForm/>
-    </div>;
-  
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
 }
 
 export default App;
--- todo/src/components/todo.jsx
+++ todo/src/components/todo.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 
-
-
-const todo = ({ todo }) => {
+const todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
-    <div className="content">
-      <p>{todo.text}</p>
-      <p className="category">({todo.category})</p>
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      <div className="content">
+        <p>{todo.text}</p>
+        <p className="category">({todo.category})</p>
+      </div>
+      <div>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
+        <button className="remove" onClick={() => removeTodo(todo.id)}>
+          X
+        </button>
+      </div>
     </div>
-    <div>
-      <button className="complete">Completar</button>
-      <button className="remove">x</button>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default todo
+export default todo;
